daf31b3d06adeae23002e38343df8373310a4a3d
otaviolube <otaviolube@gmail.com>
2021-12-29 14:45:31 -0300

    Refatorando estrutura do projeto

--- app/plus/jwtToken.js
+++
@@ -1,2 +0,0 @@
-
- module.exports = jwt = require('jsonwebtoken');
--- config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    username: 'root',
-    password: '@connect',
-    database: 'controlecompet',
-    host: 'localhost',
-    port: '3306',
-    dialect: 'mysql',
-}
