667d6367d1a70722782941d5448ac963ebe8a87b
otaviolube <otaviolube@gmail.com>
2022-01-04 15:04:40 -0300

    Infraestrutura dos databases criadas

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     define: {
         timestamps: true,
