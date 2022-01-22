dd40e43b7e040884324d9a65fe416072840f91bb
otaviolube <otaviolube@gmail.com>
2022-01-21 22:26:30 -0300

    Adicionando controller de cliente

---
+++ src/app/controllers/ClientController.js
@@ -0,0 +1,31 @@
+const ClientModel = require('../models/ClientModel');
+
+class ClientController {
+    
+    constructor(){
+
+    }
+
+    async getClients(req, res){
+
+    }
+
+    async getClient(req, res){
+
+    }
+
+    async createClient(req, res){
+
+    }
+
+    async updateCliente(req, res){
+
+    }
+
+    async deleteClient(req, res){
+
+    }
+
+}
+
+module.exports = new ClientController();
