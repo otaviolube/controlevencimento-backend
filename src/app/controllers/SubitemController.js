dacc74ff2770102bd7851347e051528c07e28a1d
otaviolube <otaviolube@gmail.com>
2022-01-21 21:53:50 -0300

    Adicionando controlador de subitems

---
+++ src/app/controllers/SubitemController.js
@@ -0,0 +1,31 @@
+const SubitemModel = require('../models/SubitemModel');
+
+class SubitemController {
+
+    constructor(){
+
+    }
+
+    async getSubitems(req, res){
+
+    }
+
+    async getSubitem(req, res){
+
+    }
+
+    async createSubitem(req, res){
+
+    }
+
+    async updateSubitem(req, res){
+
+    }
+
+    async deleteSubitem(req, res){
+
+    }
+
+}
+
+module.exports = new SubitemController();
