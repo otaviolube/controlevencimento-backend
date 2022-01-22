9ba93760f54bf2a9d7e5436e098074783b771b14
otaviolube <otaviolube@gmail.com>
2022-01-21 23:51:15 -0300

    Adicionando o controlador de expirations

---
+++ src/app/controllers/ExpirationModel.js
@@ -0,0 +1,31 @@
+const ExpirationModel = require('../models/ExpirationModel');
+
+class ExpirationController {
+    
+    constructor() {
+
+    }
+
+    async getExpirations(req, res){
+
+    }
+
+    async getExpiration(req, res){
+
+    }
+
+    async createExpiration(req, res){
+
+    }
+
+    async updateExpiration(req, res){
+
+    }
+
+    async deleteExpiration(req, res){
+
+    }
+
+}
+
+module.exports = new ExpirationController();
