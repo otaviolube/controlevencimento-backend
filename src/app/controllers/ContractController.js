7a4d4334288148b6ef4c0b60ebd710404cef43e4
otaviolube <otaviolube@gmail.com>
2022-01-21 23:27:38 -0300

    Criando o controlador para contratos

---
+++ src/app/controllers/ContractController.js
@@ -0,0 +1,31 @@
+const ContractModel = require('../models/ContractModel');
+
+class ContractController {
+
+    constructor(){
+
+    }
+
+    async getContracts(req, res){
+
+    }
+
+    async getContract(req, res){
+
+    }
+
+    async creatContract(req, res){
+
+    }
+
+    async updateContract(req, res){
+
+    }
+
+    async deleteContract(req, res){
+
+    }
+    
+}
+
+module.exports = new ContractController();
