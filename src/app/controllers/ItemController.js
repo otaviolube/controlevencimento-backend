7abacf26971d826a94d69b80fa35ceae053d94ec
otaviolube <otaviolube@gmail.com>
2022-01-21 21:20:13 -0300

    Inserindo chamadas de métodos do controlador de items

--- src/app/controllers/ItemController.js
+++ src/app/controllers/ItemController.js
@@ -1,8 +1,30 @@
-
+const ItemModel = require('../models/ItemModel');
 class ItemController {
+    
     constructor(){
 
     }
+
+    async createItem(req, res){
+
+    }
+
+    async showItems(req, res){
+
+    }
+
+    async showItem(req, res){
+
+    }
+
+    async updateItem(req, res){
+
+    }
+
+    async deleteItem(req, res){
+
+    }
+
 }
 
 module.exports = new ItemController();
